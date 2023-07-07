3dd505769d2316c8f219a9001673cb8fe4dc62f9
mgmckinn <mgmckinn2325@me.com>
2023-07-07 12:15:16 -0400

    load packages, fix package errors, add html, Dylan s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faCrown } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+import Colorful from "react-colorful";
+
+const Wrapper = styled.div`
+  text-align: center;
+  font-family: "Arial", sans-serif;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  color: #333;
+  margin-bottom: 30px;
+`;
+
+const StyledButton = styled.button`
+  background-color: #ff6633;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  margin-top: 20px;
+  cursor: pointer;
+`;
+
+const NameContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+
+  p {
+    margin-left: 10px;
+  }
+`;
+
+const ManagerIcon = styled(FontAwesomeIcon)`
+  color: #ff6633;
+  margin-right: 5px;
+`;
+
+const UserIcon = styled(FontAwesomeIcon)`
+  color: #333;
+  margin-right: 5px;
+`;
 
 function App() {
+  const [names, setNames] = useState([]);
+
+  const generateNames = (includeDylan) => {
+    const options = includeDylan ? names.concat("Dylan") : names;
+    let randomNames = [];
+
+    while (randomNames.length < (includeDylan ? 5 : 4)) {
+      const randomName = options[Math.floor(Math.random() * options.length)];
+
+      if (!randomNames.includes(randomName)) {
+        randomNames.push(randomName);
+      }
+    }
+
+    let randomNamesWithManagers = randomNames.map((name, index) => {
+      if (index === 0) {
+        return {
+          name: name,
+          isManager: true,
+        };
+      } else {
+        return {
+          name: name,
+          isManager: false,
+        };
+      }
+    });
+
+    setNames(randomNamesWithManagers);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Random Name App</Title>
+      <StyledButton onClick={() => generateNames(false)}>
+        Generate 4 Random Names
+      </StyledButton>
+      <StyledButton onClick={() => generateNames(true)}>
+        Generate 5 Random Names with Dylan
+      </StyledButton>
+      {names.map((item, index) => (
+        <NameContainer key={index}>
+          {item.isManager ? (
+            <ManagerIcon icon={faCrown} />
+          ) : (
+            <UserIcon icon={faUser} />
+          )}
+          <p>{item.name}</p>
+        </NameContainer>
+      ))}
+    </Wrapper>
   );
 }
 
 export default App;
+
