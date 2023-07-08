a443d5e77c9ccf137793f045779204f3cb346dbe
mgmckinn <mgmckinn2325@me.com>
2023-07-08 10:43:07 -0400

    update DOM formetting

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
